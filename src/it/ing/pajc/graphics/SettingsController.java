e4a246efdee2f86eb1c0c95b44d10ed6d7242261
1Pav1 <45851763+1Pav1@users.noreply.github.com>
2019-07-31 09:58:10 +0200

    Changes SettingsController Color changed to blue theme Animation testing

---
+++ src/it/ing/pajc/graphics/SettingsController.java
@@ -0,0 +1,37 @@
+package it.ing.pajc.graphics;
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+
+public class SettingsController {
+    private double x,y;
+    public static Stage primaryStage;
+
+    public static void setPrimaryStage(Stage primaryStage) {
+        SettingsController.primaryStage = primaryStage;
+    }
+
+    public void back() throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("Home.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Checker main menu");
+        primaryStage.initStyle(StageStyle.UNDECORATED);
+        //we gonna drag the frame
+        root.setOnMousePressed(event -> {
+            x = event.getSceneX();
+            y = event.getSceneY();
+        });
+
+        root.setOnMouseDragged(event -> {
+            primaryStage.setX(event.getScreenX() - x);
+            primaryStage.setY(event.getScreenY() - y);
+        });
+
+    }
+}
